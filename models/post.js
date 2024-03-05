d6e4f6e1cc404118382fb22c13814a3f7922f4ed
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-06 04:09:50 +1100

    updated post.js in models

--- models/post.js
+++ models/post.js
@@ -1,47 +1,64 @@
+// 1. Import necessary modules
+// - Import the `Model` and `DataTypes` classes from Sequelize for model creation and defining data types.
+// - Import the Sequelize connection from the `/config/connection` file.
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+// 2. Define the Post model class
 class Post extends Model {}
 
+// 3. Initialise the Post model with attributes and options
 Post.init(
+  // Define the model's attributes
   {
+    // Primary key, set as an auto-incrementing integer
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+
+    // Unique post title with a maximum length of 25 characters
     title: {
       type: DataTypes.STRING(25),
       allowNull: false,
       unique: true,
     },
+
+    // Unique post content
     content: {
       type: DataTypes.TEXT,
       allowNull: false,
       unique: true,
     },
+    // Date the post was created, set to current date by default
     date_created: {
-      type: DataTypes.DATEONLY,
+      type: DataTypes.DATEONLY, // Stores only date (without time)
       allowNull: false,
-      defaultValue: DataTypes.NOW,
+      defaultValue: DataTypes.NOW, // Sets the default value to the current date
     },
+    // Foreign key referencing the user who created the post
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'user',
-        key: 'id',
+        model: 'user', // References the 'user' model
+        key: 'id', // References the 'id' field in the 'user' model
       },
     },
   },
 
+  // Options for the model
   {
+    // Use the configured database connection
     sequelize,
+    // Disable timestamps (createdAt and updatedAt)
     timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post',
+    freezeTableName: true, // Prevent Sequelize from adding pluralisation
+    underscored: true, // Use snake_case for attributes
+    modelName: 'post', // Set the model name
   },
 );
 
+// Export the Post model for use in other parts of the application
 module.exports = Post;
