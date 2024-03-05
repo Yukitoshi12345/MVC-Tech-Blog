809cb8b985384d12a6c5bd6ed0ed057e28ceaeee
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-06 02:37:40 +1100

    completed index.js connection between tables with PK FK

--- models/index.js
+++ models/index.js
@@ -1,6 +1,37 @@
 // User has many post
-// User has many comments
 // Post belong to user
 // Post has many comments
 // Comments belong to user
+// User has many comments
 // Comment belong to post
+
+const User = require('./user');
+const Comment = require('./comment');
+const Post = require('./post');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+module.exports = { Comment, Post, User };
