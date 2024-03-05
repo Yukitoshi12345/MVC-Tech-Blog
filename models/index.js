1a2b9f2708909f78123fca401725dcc7bcec33f9
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-06 04:12:59 +1100

    updated index.js in models

--- models/index.js
+++ models/index.js
@@ -5,33 +5,43 @@
 // User has many comments
 // Comment belong to post
 
+// Import the necessary models for defining relationships
 const Comment = require('./comment');
 const Post = require('./post');
 const User = require('./user');
 
+// Establish relationships between models:
+
+// 1.  User has many posts:
 User.hasMany(Post, {
-  foreignKey: 'user_id',
+  foreignKey: 'user_id', // Specify the foreign key in the Post model that references the User model
 });
 
+// 2.  Post belongs to a user:
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
+  foreignKey: 'user_id', // Specify the foreign key in the Post model that references the User model
 });
 
+// 3.  Post has many comments:
 Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'post_id', // Specify the foreign key in the Comment model that references the Post model
+  onDelete: 'CASCADE', // Delete associated comments when a post is deleted
 });
 
+// 4.  Comment belongs to a user:
 Comment.belongsTo(User, {
-  foreignKey: 'user_id',
+  foreignKey: 'user_id', // Specify the foreign key in the Comment model that references the User model
 });
 
+// 5.  User has many comments:
 User.hasMany(Comment, {
-  foreignKey: 'user_id',
+  foreignKey: 'user_id', // Specify the foreign key in the Comment model that references the User model
 });
 
+// 6.  Comment belongs to a post:
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
+  foreignKey: 'post_id', // Specify the foreign key in the Comment model that references the Post model
 });
 
+// Export the models for use in other parts of the application
 module.exports = { Comment, Post, User };
