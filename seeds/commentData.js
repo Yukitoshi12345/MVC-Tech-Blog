96c69719de158cdaf28c436ae2ce2e5c4defacad
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-06 10:30:20 +1100

    updated commentData seeds

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,30 +1,41 @@
+// Import the Comment model from the '../models' directory
 const { Comment } = require('../models');
 
+// Define an array of comment data, each object representing a new comment
 const commentData = [
+  // Comment 1: Positive and insightful response
   {
-    content: 'I never thought of it that way, but it makes perfect sense!',
-    user_id: '2',
-    post_id: '1',
+    content: 'I never thought of it that way, but it makes perfect sense!', // Comment text
+    user_id: '2', // ID of the user who posted the comment
+    post_id: '1', // ID of the post this comment belongs to
   },
+
+  // Comment 2: Generic and uninformative response
   {
-    content: 'Thats something I guess...',
-    user_id: '2',
-    post_id: '2',
+    content: 'Thats something I guess...', // Comment text
+    user_id: '2', // ID of the user who posted the comment
+    post_id: '2', // ID of the post this comment belongs to
   },
+
+  // Comment 3: Appreciative and informative response
   {
     content:
-      'This is such a well-written and insightful post. I learned a lot!',
-    user_id: '1',
-    post_id: '2',
+      'This is such a well-written and insightful post. I learned a lot!', // Comment text
+    user_id: '1', // ID of the user who posted the comment
+    post_id: '2', // ID of the post this comment belongs to
   },
+
+  // Comment 4: Appreciative of unique perspective
   {
     content:
-      "Thank you for sharing your unique perspective on this topic. It's refreshing to see a different viewpoint",
-    user_id: '3',
-    post_id: '1',
+      "Thank you for sharing your unique perspective on this topic. It's refreshing to see a different viewpoint", // Comment text
+    user_id: '3', // ID of the user who posted the comment
+    post_id: '1', // ID of the post this comment belongs to
   },
 ];
 
+// Define a function `seedComment` to create comments in bulk
 const seedComment = () => Comment.bulkCreate(commentData);
 
+// Use the Comment model to create multiple comments in one operation using 'bulkCreate'
 module.exports = seedComment;
